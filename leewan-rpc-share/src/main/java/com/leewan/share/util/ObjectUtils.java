c4197d47fa19801d87b0063fa7ba708fa1fe3ba2
jackycjw <jackycjw@sina.cn>
2021-12-29 23:21:04 +0800

    工具丰富

---
+++ leewan-rpc-share/src/main/java/com/leewan/share/util/ObjectUtils.java
@@ -0,0 +1,8 @@
+package com.leewan.share.util;
+
+public class ObjectUtils {
+
+    public static boolean isEmpty(Object[] array){
+        return array==null || array.length == 0;
+    }
+}
--- leewan-rpc-share/src/main/java/com/leewan/share/util/StringUtils.java
+++ leewan-rpc-share/src/main/java/com/leewan/share/util/StringUtils.java
@@ -2,4 +2,12 @@ package com.leewan.share.util;
 
 public class StringUtils {
 
+    public static boolean hasText(String text){
+        return text != null && text.length()>0 ;
+    }
+
+    public static boolean hasLength(String text){
+        return hasText(text);
+    }
+
 }
